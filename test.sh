f9c8be75d6c7f80bf698e59990341b541821fa73
Matteo Medioli <31959430+matteomedioli@users.noreply.github.com>
2020-01-15 12:32:53 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,4 @@
-#!/bin/bash                                                                                         \
-                                                                                                     
-
-# Declare an array of parallel activities NW                                                        \
-                                                                                                     
-declare -a par_deg=()
-declare -a n=("512000" "1024000" "2048000" "4096000" "8192000")
-
-# Iterate the string array using for loop                                                           \
-                                                                                                     
-#!/bin/bash                                                                                         \
-                                                                                                     
+#!/bin/bash
 for par_deg in 1 4 8 16 32 64 128
 do
     for n in 512000 1024000 2048000 4096000 8192000
