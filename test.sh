e99a911e8faecf839ae79b292bf9039e1d8e214b
Matteo Medioli <31959430+matteomedioli@users.noreply.github.com>
2020-01-15 12:07:55 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
- 
-# Declare an array of parallel activities NW
+#!/bin/bash                                                                                          
+
+# Declare an array of parallel activities NW                                                         
 declare -a par_deg=()
 declare -a n=("512000" "1024000" "2048000" "4096000" "8192000")
- 
-# Iterate the string array using for loop
-#!/bin/bash
+
+# Iterate the string array using for loop                                                            
+#!/bin/bash                                                                                          
 for par_deg in 1 4 8 16 32 64 128
 do
     for n in 512000 1024000 2048000 4096000 8192000
     do
-        ./a.out $n $par_deg > ./test/$n"_"$par_deg.txt
+        ./main $n $par_deg >> ./test/result.csv
     done
 done
 exit 0
